6115b46baab9894e3401f4a64139f1f9d9945ed2
usrhere <4880627+usrhere@users.noreply.github.com>
2026-08-02 14:06:02 +0200

    Solution for task 4.

--- gmachine.go
+++ gmachine.go
@@ -1,2 +1,41 @@
 // Package gmachine implements a simple virtual CPU, known as the G-machine.
 package gmachine
+
+type Word uint64
+
+const (
+	DefaultMemSize = 1
+	OpHALT         = 0
+	OpNOOP         = 1
+	OpINCA         = 2
+	OpDECA         = 3
+)
+
+type Machine struct {
+	P      Word // Program counter
+	A      Word // Accumulator
+	Memory []Word
+}
+
+func (m *Machine) Run() {
+instructions:
+	for _, v := range m.Memory {
+		m.P++
+		switch v {
+		case 0:
+			break instructions
+		case 2:
+			m.A++
+		case 3:
+			m.A--
+		}
+	}
+}
+
+func New() Machine {
+	m := Machine{
+		P:      0,
+		Memory: []Word{0},
+	}
+	return m
+}
--- gmachine_test.go
+++ gmachine_test.go
@@ -10,12 +10,64 @@ func TestNew(t *testing.T) {
 	t.Parallel()
 	g := gmachine.New()
 	var wantP gmachine.Word = 0
+	var wantA gmachine.Word = 0
 	if wantP != g.P {
 		t.Errorf("want initial P value %d, got %d", wantP, g.P)
+	}
+	if wantA != g.A {
+		t.Errorf("want initial A value %d, got %d", wantA, g.A)
 	}
 	var wantMemValue gmachine.Word = 0
 	gotMemValue := g.Memory[gmachine.DefaultMemSize-1]
 	if wantMemValue != gotMemValue {
 		t.Errorf("want last memory location to contain %d, got %d", wantMemValue, gotMemValue)
+	}
+}
+
+func TestHALT(t *testing.T) {
+	g := gmachine.New()
+	g.Run()
+	if g.P != 1 {
+		t.Errorf("want P == 1, got %d", g.P)
+	}
+}
+
+func TestNOOP(t *testing.T) {
+	g := gmachine.New()
+	g.Memory[0] = gmachine.OpNOOP
+	g.Memory = append(g.Memory, gmachine.OpHALT)
+	g.Run()
+	if g.P != 2 {
+		t.Errorf("want P == 2, got %d", g.P)
+	}
+}
+
+func TestINCA(t *testing.T) {
+	g := gmachine.New()
+	g.Memory[0] = gmachine.OpINCA
+	g.Run()
+	if g.A != 1 {
+		t.Errorf("want A == 1, got %d", g.A)
+	}
+}
+
+func TestDECA(t *testing.T) {
+	g := gmachine.New()
+	g.A = 2
+	g.Memory[0] = gmachine.OpDECA
+	g.Run()
+	if g.A != 1 {
+		t.Errorf("want A == 1, got %d", g.A)
+	}
+}
+
+func TestSubtraction(t *testing.T) {
+	g := gmachine.New()
+	g.A = 3
+	g.Memory[0] = gmachine.OpDECA
+	g.Memory = append(g.Memory, gmachine.OpDECA)
+	g.Run()
+	if g.A != 1 {
+		t.Errorf("want A == 1, got %d", g.A)
 	}
 }

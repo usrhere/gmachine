4d508de82d1ee562a1cb33f48ec3727c4ad3d1d0
usrhere <4880627+usrhere@users.noreply.github.com>
2026-08-07 17:38:53 +0200

    Start to convert it to rx82 emulator.

--- gmachine.go
+++ gmachine.go
@@ -1,50 +1,54 @@
 // Package gmachine implements a simple virtual CPU, known as the G-machine.
 package gmachine
 
-type Word uint64
-
 const (
 	DefaultMemSize = 1
 	OpHALT         = 0
 	OpNOOP         = 1
-	OpINCA         = 2
-	OpDECA         = 3
-	OpSETA         = 4
+	OpINCA         = 0x30
+	OpDECA         = 0x40
+	OpLDA          = 0x10
+	OpINCB         = 0x31
+	OpDECB         = 0x41
+	OpLDB          = 0x11
 )
 
 type Machine struct {
-	P      Word // Program counter
-	A      Word // Accumulator
-	Memory []Word
+	PC     byte // Program counter
+	A      byte // Accumulator
+	Memory []byte
 }
 
 func (m *Machine) Run() {
 instructions:
 	for i := 0; i < len(m.Memory); i++ {
 		switch m.Memory[i] {
-		case 0:
+		case OpHALT:
 			break instructions
-		case 2:
+		case OpINCA:
 			m.A++
-		case 3:
+		case OpDECA:
 			m.A--
-		case 4:
+		case OpLDA:
 			i++
 			m.A = m.Memory[i]
 		}
 	}
-	m.P = Word(len(m.Memory))
+	m.PC = byte(len(m.Memory))
 }
 
-func (m *Machine) RunProgram(program []Word) {
+func (m *Machine) RunProgram(program []byte) {
 	m.Memory = program
 	m.Run()
 }
 
 func New() Machine {
 	m := Machine{
-		P:      0,
-		Memory: []Word{0},
+		PC:     0,
+		Memory: []byte{0},
 	}
 	return m
 }
+
+// TODO: write some code that reads a binary file and executes it
+// E.g: 0x11 99 => converted to binary
--- gmachine_test.go
+++ gmachine_test.go
@@ -9,15 +9,15 @@ import (
 func TestNew(t *testing.T) {
 	t.Parallel()
 	g := gmachine.New()
-	var wantP gmachine.Word = 0
-	var wantA gmachine.Word = 0
-	if wantP != g.P {
-		t.Errorf("want initial P value %d, got %d", wantP, g.P)
+	var wantP byte = 0
+	var wantA byte = 0
+	if wantP != g.PC {
+		t.Errorf("want initial P value %d, got %d", wantP, g.PC)
 	}
 	if wantA != g.A {
 		t.Errorf("want initial A value %d, got %d", wantA, g.A)
 	}
-	var wantMemValue gmachine.Word = 0
+	var wantMemValue byte = 0
 	gotMemValue := g.Memory[gmachine.DefaultMemSize-1]
 	if wantMemValue != gotMemValue {
 		t.Errorf("want last memory location to contain %d, got %d", wantMemValue, gotMemValue)
@@ -26,26 +26,26 @@ func TestNew(t *testing.T) {
 
 func TestHALT(t *testing.T) {
 	g := gmachine.New()
-	g.RunProgram([]gmachine.Word{gmachine.OpHALT})
-	if g.P != 1 {
-		t.Errorf("want P == 1, got %d", g.P)
+	g.RunProgram([]byte{gmachine.OpHALT})
+	if g.PC != 1 {
+		t.Errorf("want P == 1, got %d", g.PC)
 	}
 }
 
 func TestNOOP(t *testing.T) {
 	g := gmachine.New()
-	g.RunProgram([]gmachine.Word{
+	g.RunProgram([]byte{
 		gmachine.OpNOOP,
 		gmachine.OpHALT,
 	})
-	if g.P != 2 {
-		t.Errorf("want P == 2, got %d", g.P)
+	if g.PC != 2 {
+		t.Errorf("want P == 2, got %d", g.PC)
 	}
 }
 
 func TestINCA(t *testing.T) {
 	g := gmachine.New()
-	g.RunProgram([]gmachine.Word{gmachine.OpINCA})
+	g.RunProgram([]byte{gmachine.OpINCA})
 	if g.A != 1 {
 		t.Errorf("want A == 1, got %d", g.A)
 	}
@@ -54,19 +54,19 @@ func TestINCA(t *testing.T) {
 func TestDECA(t *testing.T) {
 	g := gmachine.New()
 	g.A = 2
-	g.RunProgram([]gmachine.Word{gmachine.OpDECA})
+	g.RunProgram([]byte{gmachine.OpDECA})
 	if g.A != 1 {
 		t.Errorf("want A == 1, got %d", g.A)
 	}
 }
 
 func TestSubtraction(t *testing.T) {
-	var operand, wantA gmachine.Word
+	var operand, wantA byte
 	operand = 3
 	wantA = 1
 	g := gmachine.New()
-	g.RunProgram([]gmachine.Word{
-		gmachine.OpSETA,
+	g.RunProgram([]byte{
+		gmachine.OpLDA,
 		operand,
 		gmachine.OpDECA,
 		gmachine.OpDECA,
@@ -76,9 +76,9 @@ func TestSubtraction(t *testing.T) {
 	}
 	operand = 4
 	wantA = 2
-	g.P = 0
-	g.RunProgram([]gmachine.Word{
-		gmachine.OpSETA,
+	g.PC = 0
+	g.RunProgram([]byte{
+		gmachine.OpLDA,
 		operand,
 		gmachine.OpDECA,
 		gmachine.OpDECA,
@@ -88,9 +88,9 @@ func TestSubtraction(t *testing.T) {
 	}
 	operand = 100
 	wantA = 98
-	g.P = 0
-	g.RunProgram([]gmachine.Word{
-		gmachine.OpSETA,
+	g.PC = 0
+	g.RunProgram([]byte{
+		gmachine.OpLDA,
 		operand,
 		gmachine.OpDECA,
 		gmachine.OpDECA,
@@ -101,17 +101,32 @@ func TestSubtraction(t *testing.T) {
 }
 
 func TestSETA(t *testing.T) {
-	var operand gmachine.Word = 99
+	var operand byte = 99
 	g := gmachine.New()
-	g.RunProgram([]gmachine.Word{
-		gmachine.OpSETA,
+	g.RunProgram([]byte{
+		gmachine.OpLDA,
 		operand,
 	})
-	if g.P != 2 {
-		t.Errorf("want P == 2, got %d", g.P)
+	if g.PC != 2 {
+		t.Errorf("want P == 2, got %d", g.PC)
 	}
 	if g.A != operand {
 		t.Errorf("want A == %d, got %d", operand, g.A)
 	}
 
+}
+
+func TestB(t *testing.T) {
+	var operand byte = 99
+	g := gmachine.New()
+	g.RunProgram([]byte{
+		gmachine.OpLDB,
+		operand,
+		gmachine.OpINCB,
+		gmachine.OpINCB,
+		gmachine.OpDECB,
+	})
+	if g.B != 100 {
+		t.Errorf("want B == %d, got %d", 100, g.B)
+	}
 }
